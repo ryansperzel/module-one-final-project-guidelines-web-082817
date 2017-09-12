53ca2f78127e7887f55ed6522c35c27b5c5d60f8
github username <github email address>
2017-09-12 17:36:18 -0400

    Push Six

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -2,8 +2,9 @@ require 'unirest'
 require 'colorize'
 require 'colorized_string'
 
-$p1 = ""
-match_up = []
+# $p1 = ""
+$players_array = []
+
 
 def team_data
   response = Unirest.get "https://montanaflynn-fifa-world-cup.p.mashape.com/teams",
@@ -43,18 +44,31 @@ def welcome
 \n\n"
 
 
-  puts "What is your name Player One?".colorize(:red)
-  $p1 = gets.chomp
-
-  puts "\n\n\n\nWhat is yout name Player Two".colorize(:blue)
-  $p2 = gets.chomp
+  # puts "What is your name Player One?".colorize(:red)
+  # $p1 = gets.chomp
+  #
+  # puts "\n\n\n\nWhat is yout name Player Two".colorize(:blue)
+  # $p2 = gets.chomp
 
 end
 
-def players
-  $p1_inst = Bettor.create(:name => $p1, :tokens => 20)
-  $p2_inst = Bettor.create(:name => $p2, :tokens => 20)
+def player_count
+  puts "How many players are playing?"
+  response = gets.chomp
+  response.to_i
 end
+
+def add_player(player_count)
+  counter = 0
+  while counter < player_count
+    puts "What is your name?"
+    name = gets.chomp
+    new_player = Bettor.create(:name => name, :tokens => 20)
+    $players_array << new_player
+    counter += 1
+  end
+end
+
 
 def find_team(team)
   team_data.body.find do |team_ob|
@@ -68,6 +82,7 @@ end
 
 def match
   round_counter = 1
+  player_team = {}
 
   while round_counter <= 5
     team_1 = random_team
@@ -80,45 +95,50 @@ def match
     puts "vs."
     sleep(1)
     puts "\n\n#{team_2["title"]}"
-    puts "\n\n\n#{$p1}: choose your team".colorize(:red)
-    team = gets.chomp
 
-    puts "\n\n\n#{$p2}: choose your team".colorize(:blue)
-    team2 = gets.chomp
+    $players_array.each do |player|
+      puts "#{player[:name]}: pick a team".colorize(:blue)
+      team = gets.chomp
+      player_team[player[:id]] = find_team(team)["id"]
+    end
 
-    tid = find_team(team)["id"]
-    tid2 = find_team(team2)["id"]
-    Bet.create(:bettor_id => $p1_inst[:id], :team_id => tid)
-    Bet.create(:bettor_id => $p2_inst[:id], :team_id => tid2)
+    player_team.each do |k, v|
+      Bet.create(:bettor_id => k, :team_id => v)
+    end
+
     array = [team_1["id"], team_2["id"]]
     winner = array.sample
 
-    if winner == tid && winner == tid2
-      puts "BOTH PLAYERS WON THE ROUND!"
-    elsif winner == tid && winner != tid2
-      puts "Player 1 won the round!".colorize(:red)
-      sleep(3)
-      $p2_inst.update(tokens:($p2_inst["tokens"]-10))
-      if $p2_inst["tokens"] <= 0
-        puts "PLAYER ONE WON THE GAME!".colorize(:red)
-        break
-      end
-    elsif winner == tid2 && winner != tid
-      puts "Player 2 won the round!".colorize(:blue)
-      sleep(3)
-      $p1_inst.update(tokens:($p1_inst["tokens"]-10))
-      if $p1_inst["tokens"] <= 0
-        puts "PLAYER TWO WON THE GAME!".colorize(:blue)
-        break
-      end
-    else
-      winner != tid && winner != tid2
-      puts "BOTH PLAYERS LOST THE ROUND!"
-      $p1_inst.update(tokens:($p1_inst["tokens"]-10))
-      $p2_inst.update(tokens:($p2_inst["tokens"]-10))
-    end
-    total_score
-    round_counter += 1
+    # binding pry
+  #   if winner == tid && winner == tid2
+  #     puts "BOTH PLAYERS WON THE ROUND!"
+  #   elsif winner == tid && winner != tid2
+  #     puts "Player 1 won the round!".colorize(:red)
+  #     sleep(3)
+  #     $p2_inst.update(tokens:($p2_inst["tokens"]-10))
+  #     if $p2_inst["tokens"] <= 0
+  #       puts "PLAYER ONE WON THE GAME!".colorize(:red)
+  #       break
+  #     end
+  #   elsif winner == tid2 && winner != tid
+  #     puts "Player 2 won the round!".colorize(:blue)
+  #     sleep(3)
+  #     $p1_inst.update(tokens:($p1_inst["tokens"]-10))
+  #     if $p1_inst["tokens"] <= 0
+  #       puts "PLAYER TWO WON THE GAME!".colorize(:blue)
+  #       break
+  #     end
+  #   else
+  #     winner != tid && winner != tid2
+  #     puts "BOTH PLAYERS LOST THE ROUND!"
+  #     $p1_inst.update(tokens:($p1_inst["tokens"]-10))
+  #     $p2_inst.update(tokens:($p2_inst["tokens"]-10))
+  #   end
+  #   total_score
+  binding.pry
+  counter += 1
+
+  puts "poo"
   end
 end
 
