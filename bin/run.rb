229168b5919045d4dbb3b1936cda16cdaab64ca1
github username <github email address>
2017-09-13 14:15:57 -0400

    Push Eight

--- bin/run.rb
+++ bin/run.rb
@@ -2,11 +2,18 @@ require_relative '../config/environment'
 
 welcome
 
+puts "_________________________________________________________________________\n\n\n"
 # binding pry
 count = player_count
 
+
+
 add_player(count)
+
+puts "\n\n\n_________________________________________________________________________\n\n\n"
 match
+puts "\n\n\n_________________________________________________________________________\n\n\n"
+game_winner
 
 
 
